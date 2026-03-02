52e043331503d1fc58df80c8c89ad0d8c82877db
KristenKaiser <kristenmkaiser@gmail.com>
2026-03-02 14:39:38 -0800

    implemented keypad

--- bad-ideas-game-jam-2026/Scripts/Button.gd
+++ bad-ideas-game-jam-2026/Scripts/Button.gd
@@ -5,7 +5,7 @@ class_name Button3D
 @export var label : Label3D
 enum Functionality {NUMBER, BACKSPACE, CLEAR}
 @export var functionality : Functionality = Functionality.NUMBER
-signal num_button_pressed(num : int)
+signal num_button_pressed(num : String)
 signal backspace_button_pressed
 signal clear_button_pressed
 @export var color : Color #= Color.GREEN
@@ -27,8 +27,14 @@ func update_color():
 func on_press():
 	match functionality:
 		Functionality.NUMBER:
-			num_button_pressed.emit(int(text))
+			num_button_pressed.emit(text)
 		Functionality.BACKSPACE:
 			backspace_button_pressed.emit()
 		Functionality.CLEAR:
 			clear_button_pressed.emit()
+
+
+func _on_area_3d_input_event(_camera: Node, event: InputEvent, _event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			on_press()
--- bad-ideas-game-jam-2026/Scripts/keypad.gd
+++ bad-ideas-game-jam-2026/Scripts/keypad.gd
@@ -3,12 +3,26 @@ extends Node3D
 class_name KeyPad
 
 @export var buttons: Array[Button3D]
+@export var display_label : Label3D
+var keypad_input : String = ""
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	display_label.text = keypad_input
+	for button in buttons:
+		button.backspace_button_pressed.connect(backspace)
+		button.clear_button_pressed.connect(clear_input)
+		button.num_button_pressed.connect(append_num)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func backspace():
+	keypad_input = keypad_input.left(keypad_input.length()-1)
+	display_label.text = keypad_input
+	
+func clear_input():
+	keypad_input = ""
+	display_label.text = keypad_input
+
+func append_num(value : String):
+	keypad_input += value
+	display_label.text = keypad_input
