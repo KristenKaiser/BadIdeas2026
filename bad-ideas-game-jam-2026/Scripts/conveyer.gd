8953d283de1dd280c406dd6959138063909013d3
KristenKaiser <kristenmkaiser@gmail.com>
2026-03-04 09:29:13 -0800

    converted conveyer to flat layout

--- bad-ideas-game-jam-2026/Scripts/conveyer.gd
+++ bad-ideas-game-jam-2026/Scripts/conveyer.gd
@@ -1,5 +1,10 @@
 extends CSGBox3D
 
+@export var collision_shape : CollisionShape3D 
+
+func _ready() -> void:
+	collision_shape.shape.size = size
+	
 
 func _on_area_3d_area_entered(area: Area3D) -> void:
 	if area.get_parent() is Box:
--- bad-ideas-game-jam-2026/Scripts/floor.gd
+++ bad-ideas-game-jam-2026/Scripts/floor.gd
@@ -3,6 +3,9 @@ extends CSGBox3D
 
 
 
-func _on_area_3d_area_exited(area: Area3D) -> void:
+#
+
+
+func _on_area_3d_area_entered(area: Area3D) -> void:
 	if area.get_parent() is Box:
 		area.get_parent().ship()
