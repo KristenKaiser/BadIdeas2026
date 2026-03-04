48fc0d0b986bcdaa1133fd3bbd2831bf699339a4
KristenKaiser <kristenmkaiser@gmail.com>
2026-03-04 14:37:28 -0800

    boxes no longer sink into conveyer

--- bad-ideas-game-jam-2026/Scripts/conveyer.gd
+++ bad-ideas-game-jam-2026/Scripts/conveyer.gd
@@ -2,6 +2,7 @@ extends CSGBox3D
 
 @export var collision_shape : CollisionShape3D 
 
+
 func _ready() -> void:
 	collision_shape.shape.size = size
 
@@ -9,6 +10,7 @@ func _ready() -> void:
 func entered_conveyer(area: Area3D) -> void:
 	if area.get_parent() is Box:
 		area.get_parent().current_state = area.get_parent().State.CONVEYING
+		area.get_parent().global_position.y = global_position.y + size.y/2 + area.get_parent().size.y/2
 
 
 func exited_conveyer(area: Area3D) -> void:
