31c3b65a59e401bed0267662321a4ac24c0cf3e9
KristenKaiser <kristenmkaiser@gmail.com>
2026-03-08 12:56:24 -0700

    implemented trash compactor

--- bad-ideas-game-jam-2026/Scripts/trash_compactor.gd
+++ bad-ideas-game-jam-2026/Scripts/trash_compactor.gd
@@ -1,11 +1,11 @@
 extends CSGBox3D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_area_3d_input_event(_camera: Node, event: InputEvent, _event_position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			if Global.camera_manager.held_object != null and Global.camera_manager.held_object is TrashBag: 
+				Global.camera_manager.held_object.queue_free()
+				Global.camera_manager.held_object = null
